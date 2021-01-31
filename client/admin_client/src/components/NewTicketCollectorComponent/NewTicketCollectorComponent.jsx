cc3a8699ae7af641d12ded25277545b01d5e2114
Shashank Biplav <Biplavshashank7@gmail.com>
2021-01-31 12:00:43 +0530

    [Feature: Client: Admin] Added extra fields to add a new TC

--- client/admin_client/src/components/NewTicketCollectorComponent/NewTicketCollectorComponent.jsx
+++ client/admin_client/src/components/NewTicketCollectorComponent/NewTicketCollectorComponent.jsx
@@ -6,16 +6,24 @@ function NewTicketCollectorComponent() {
   const [isLoading, setIsLoading] = useState(false);
   //login admin
   const createNewTicketCollector = async () => {
-    setIsLoading((isLoading) => !isLoading);
+    setIsLoading(true);
     const phone = document.getElementById("phone").value;
-    if (!phone) {
+    const name = document.getElementById("name").value;
+    const password = document.getElementById("password").value.trim();
+    const confirmPassword = document.getElementById("confirmPassword").value.trim();
+    if (!phone || !name || !password || !confirmPassword) {
       alert("Empty");
-      setIsLoading((isLoading) => !isLoading);
+      setIsLoading(false);
+      return;
+    }
+    if(password !== confirmPassword){
+      alert("Passwords do not match!");
+      setIsLoading(false);
       return;
     }
     const url = "https://intelli-park.herokuapp.com/auth/ticket-collector/signup/phone";
     try {
-       await Axios.post(url, {
+      await Axios.post(url, {
         "phone": phone
       }, {
         headers: {
@@ -35,14 +43,26 @@ function NewTicketCollectorComponent() {
       <div className="container px-8 pt-48 pb-24 mx-auto lg:px-4">
         <div
           className="flex flex-col w-full p-8 mx-auto mt-10 border rounded-lg lg:w-2/6 md:w-1/2 md:ml-auto md:mt-0">
-              <div className="relative ">
-                <input type="number" id="phone" name="phone" placeholder="Phone Number"
-                       className="w-full px-4 py-2 mb-4 mr-4 text-base text-blue-700 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0"/>
-              </div>
-          {!isLoading ?<button onClick={createNewTicketCollector}
-                   className="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-blue-700 hover:from-blue-600 to-blue-600 hover:to-blue-700 focus:ring focus:outline-none">ADD
+          <div className="relative ">
+            <input type="text" id="name" name="name" placeholder="Name"
+                   className="w-full px-4 py-2 mb-4 mr-4 text-base text-blue-700 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0"/>
+          </div>
+          <div className="relative ">
+            <input type="number" id="phone" name="phone" placeholder="Phone Number"
+                   className="w-full px-4 py-2 mb-4 mr-4 text-base text-blue-700 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0"/>
+          </div>
+          <div className="relative ">
+            <input type="password" id="password" name="password" placeholder="Password"
+                   className="w-full px-4 py-2 mb-4 mr-4 text-base text-blue-700 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0"/>
+          </div>
+          <div className="relative ">
+            <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password"
+                   className="w-full px-4 py-2 mb-4 mr-4 text-base text-blue-700 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0"/>
+          </div>
+          {!isLoading ? <button onClick={createNewTicketCollector}
+                                className="px-8 py-2 font-semibold text-white transition duration-500 ease-in-out transform rounded-lg shadow-xl bg-gradient-to-r from-blue-700 hover:from-blue-600 to-blue-600 hover:to-blue-700 focus:ring focus:outline-none">ADD
             NEW TICKET COLLECTOR
-          </button>: <LoadingSpinner />}
+          </button> : <LoadingSpinner/>}
         </div>
       </div>
     </section>
