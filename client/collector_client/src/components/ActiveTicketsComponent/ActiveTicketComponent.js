939063926449f661c8ba1bab658f0482545e54db
Shashank Biplav <Biplavshashank7@gmail.com>
2021-02-03 16:37:57 +0530

    [Feature:Client:Collector] Updated Parking and Active tickets component

--- client/collector_client/src/components/ActiveTicketsComponent/ActiveTicketComponent.js
+++ client/collector_client/src/components/ActiveTicketsComponent/ActiveTicketComponent.js
@@ -20,8 +20,11 @@ function ActiveTicketComponent (props) {
         </h2>
         <h2 className="mb-3 text-lg font-semibold text-gray-700 lg:text-xl title-font"> ₹ {(Math.round(data.amount *100)/100).toFixed(2)}
         </h2>
-        <p className="mb-4 text-base leading-relaxed">CREATED BY: {data.createdBy['phone']}</p>
-        <p className="mb-4 text-base leading-relaxed">STARTING TIME: {getDateTime(data.startingTime)}</p>
+        <h3 className="mb-3">Owner Contact: {data.ownerPhone}</h3>
+        <p className="mb-4 text-base leading-relaxed">CREATED BY: <br/> {data.createdBy['name']} | {data.createdBy['phone']}</p>
+        <p className="mb-4 text-base leading-relaxed">STARTING TIME: <br/> {getDateTime(data.startingTime)}</p>
+        {!data.isActive?<p className="mb-4 text-base leading-relaxed">ENDING TIME: <br/> {getDateTime(data.endingTime)}</p> : null}
+        {data.isActive?<p className="mb-4 text-base leading-relaxed"> AMOUNT NOT COLLECTED YET</p>: <p className="mb-4 text-base leading-relaxed">COLLECTED BY: <br/> {data.collectedBy['name']} | {data.collectedBy['phone']}</p>}
         <p className="mb-4 text-base leading-relaxed">VEHICLE TYPE: {data.vehicleType} Wheeler</p>
         
           <button onClick={() =>  props.endActiveTicket(data._id)}
