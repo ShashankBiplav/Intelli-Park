0785bcc06e913e82ac9e6dc92027060716cbc2e4
Shashank Biplav <Biplavshashank7@gmail.com>
2021-02-03 15:28:58 +0530

    [Feature:Client:Admin] Added extra info for active tickets

--- client/admin_client/src/components/ActiveTicketsComponent/ActiveTicketComponent.jsx
+++ client/admin_client/src/components/ActiveTicketsComponent/ActiveTicketComponent.jsx
@@ -15,12 +15,13 @@ function ActiveTicketComponent (props) {
   return (
     <div className= {"m-6 w-full lg:w-1/5  rounded-lg shadow-lg bg-green-300"}>
       <div className="p-6 object-cover object-center">
-        <h2 className="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"> {data.vehicleNumber}
+        <h2 className="mb-3 text-lg font-semibold text-gray-800 lg:text-2xl title-font"> {data.vehicleNumber}
         </h2>
         <h2 className="mb-3 text-lg font-semibold text-gray-700 lg:text-xl title-font"> ₹ {(Math.round(data.amount *100)/100).toFixed(2)}
         </h2>
-        <p className="mb-4 text-base leading-relaxed">CREATED BY: {data.createdBy['phone']}</p>
-        <p className="mb-4 text-base leading-relaxed">STARTING TIME: {getDateTime(data.startingTime)}</p>
+        <h3 className="mb-3">Owner Contact: {data.ownerPhone}</h3>
+        <p className="mb-4 text-base leading-relaxed">CREATED BY: <br/> {data.createdBy['name']} | {data.createdBy['phone']}</p>
+        <p className="mb-4 text-base leading-relaxed">STARTING TIME: <br/> {getDateTime(data.startingTime)}</p>
         <p className="mb-4 text-base leading-relaxed">VEHICLE TYPE: {data.vehicleType} Wheeler</p>
       </div>
     </div>
